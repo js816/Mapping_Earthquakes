df9264db11d9bf40939281485a9f79dc36b2e209
unknown <82730954+js816@users.noreply.github.com>
2021-07-19 14:34:42 -0500

    Adding orange markers on dark map

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -11,7 +11,10 @@ let cityData = cities;
 cityData.forEach(function(city) {
     console.log(city)
     L.circleMarker(city.location, {
-        radius: city.population/100000
+        radius: city.population/200000,
+        color: 'orange',
+        // Adapting code found here to adjust line weight:  https://gis.stackexchange.com/questions/202392/change-width-of-outline-for-leaflet-map-depending-on-zoomlevel
+        weight: 4
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map);
@@ -28,7 +31,7 @@ cityData.forEach(function(city) {
  */
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
