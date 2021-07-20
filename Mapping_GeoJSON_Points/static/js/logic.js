755a76c1f8415eb4ca7bd96fbcd067edb62d5857
unknown <82730954+js816@users.noreply.github.com>
2021-07-20 12:58:20 -0500

    Adding working GeoJSON logic file

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,9 +1,41 @@
 // Add console.log to check to see if our code is working.
-console.log("working for multiples");
+console.log("working for GeoJSON");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng)
+      .bindPopup("<h2>" + feature.properties.city + "</h2>")
+    }
+
+  }).addTo(map);
+
+/*
 // Get data from cities.js
 let cityData = cities;
 
@@ -20,7 +52,7 @@ cityData.forEach(function(city) {
     .addTo(map);
    });;
 
-/*
+
  // Add a light-yellow marker with 300-meter radius of Central Los Angeles on a dark map
  L.circleMarker([34.0522, -118.2437], {
      color: 'black',
@@ -31,7 +63,7 @@ cityData.forEach(function(city) {
  */
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
